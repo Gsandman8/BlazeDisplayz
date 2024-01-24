f858f54d4845cb6acf58d77ba8b833cab6f3026b
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-01-24 02:40:51 -0800

    added new arrivals tab, updated styling for nav and header

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -79,42 +79,47 @@ export default function Nav() {
   }
 
   return (
-    <ul className="nav justify-content nav-tabs-container">
+    <ul className="nav nav-tabs-container">
+      <li className='nav-item'>
+        <Link id='navLink' to='/newarrival' className='nav-link'>
+        NEW ARRIVAL
+        </Link>
+      </li>
       <li className="nav-item" 
       onMouseEnter={() => handleMouseEnter('mens')} 
       onMouseLeave={handleMouseLeave}
       >
         <Link id='navLink' to="/mens" className='nav-link'>
-          mens
+          MENS
         </Link>
         <ul id='menuList' className={`${isMensHovering ? '' : 'hidden'}`}>
-          <li><Link>mens hoodies</Link></li>
-          <li><Link>mens pants</Link></li>
-          <li><Link>mens shoes</Link></li>
+          <li><Link> HOODIES </Link></li>
+          <li><Link> PANTS </Link></li>
+          <li><Link> SHOES </Link></li>
         </ul>
       </li>
       <li className="nav-item" 
       onMouseEnter={() => handleMouseEnter('womens')} 
       onMouseLeave={handleMouseLeave}>
         <Link id='navLink' to="/mens" className='nav-link'>
-          womens
+          WOMENS
         </Link>
         <ul id='menuList' className={`${isWomensHovering ? '' : 'hidden'}`}>
-          <li><Link>womens hoodies</Link></li>
-          <li><Link>womens pants</Link></li>
-          <li><Link>womens shoes</Link></li>
+          <li><Link> HOODIES </Link></li>
+          <li><Link> PANTS </Link></li>
+          <li><Link> SHOES </Link></li>
         </ul>
       </li>
       <li className="nav-item" 
       onMouseEnter={() => handleMouseEnter('kids')} 
       onMouseLeave={handleMouseLeave}>
         <Link id='navLink' to="/kids" className='nav-link'>
-          kids
+          KIDS
         </Link>
         <ul id='menuList' className={`${isKidsHovering ? '' : 'hidden'}`}>
-          <li><Link>kids hoodies</Link></li>
-          <li><Link>kids pants</Link></li>
-          <li><Link>kids shoes</Link></li>
+          <li><Link> HOODIES </Link></li>
+          <li><Link> PANTS </Link></li>
+          <li><Link> SHOES</Link></li>
         </ul>
       </li>
     </ul>
