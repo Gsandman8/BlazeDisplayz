441ed92918349028c722b9d014e2f7db7d7e85d7
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-01-25 15:50:34 -0800

    Update seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1058,8 +1058,317 @@ db.once('open', async () => {
       quantity: 50,
       tags: [tags[2]._id]
     },
+    {
+      name: 'The Havanna',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product3.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Neon Nights',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product4.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Cotton Candy',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product5.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'The Set',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product6.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'The Spark',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product7.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'The Universe',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product8.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Get Fly',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product9.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Autumn',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product10.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'The Knock',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product11.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Pop Lock',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product12.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'The Bently',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product13.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Somethin New',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product14.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'The Mami',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product15.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Triple Double',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product16.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+
+
 
     //end of womens shoes
+
+    //start of woemens tops
+
+    {
+      name: 'No Play',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensTops/product1.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Hand Out',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product2.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Shutter Fly',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product3.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Airplane Mode',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product4.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Weaponized',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product6.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Show Time',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product7.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Night Rider',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product8.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Necessary',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product9.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'No Bammer',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product10.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Chewy',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product11.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'The Dash',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product12.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'The Move',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product13.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Bugatting',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product14.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Old English',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product15.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Iceberg Slim',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product16.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+    {
+      name: 'Fake ID',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: '/products/categories/womensShoes/product17.png',
+      category: categories[1]._id,
+      price: 69.99,
+      quantity: 50,
+      tags: [tags[2]._id]
+    },
+
+
+
+    //end of womens tops
 
 
 
