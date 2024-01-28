aba4a70fdb5d659a8553ed9fde2253581ef96024
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-27 22:52:02 -0800

    Repurposed Men's page code for women's and kid' pages

--- client/src/pages/Women.jsx
+++ client/src/pages/Women.jsx
@@ -1,14 +1,52 @@
-
 import ProductList from '../components/ProductList';
 import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_CURRENT_TAG } from '../utils/actions';
+import { UPDATE_CURRENT_CATEGORY, UPDATE_CURRENT_TAG } from '../utils/actions';
+import { Link } from 'react-router-dom';
+import  ManIcon  from '@mui/icons-material/Man';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { useEffect } from 'react';
 
 
 const Women = () => {
+    const [state, dispatch] = useStoreContext();
+    const tag = state.currentTag || 'Clothing';
+    
+    useEffect(() => {
+        dispatch({
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: 'Women'
+        })
+    }, [dispatch])
+    
+
     return (
         <div className="container row">
-            <h1 className="text-center">Women's Clothing</h1>
+            <a id="top"></a>
+            <nav className="navbar navbar-expand-lg justify-content-between">
+            <h1 className="text-center">Women's {tag} <ManIcon/></h1>
+                <Link onClick={()=>dispatch({
+                    type: UPDATE_CURRENT_TAG,
+                    currentTag: 'Clothing'
+                })} style={{color:"black"}}>All Clothing</Link>
+                <Link onClick={()=>dispatch({
+                    type: UPDATE_CURRENT_TAG,
+                    currentTag: 'Jackets'
+                })} style={{color:"black"}}>Hoodies</Link>
+                <Link onClick={()=>dispatch({
+                    type: UPDATE_CURRENT_TAG,
+                    currentTag: 'Pants'
+                })} style={{color:"black"}}>Pants</Link>
+                <Link onClick={()=>dispatch({
+                    type: UPDATE_CURRENT_TAG,
+                    currentTag: 'Shoes'
+                })} style={{color:"black"}}>Shoes</Link>
+            </nav>
+            <hr />
             <ProductList />
+            <br /><br /><br /><br /><br />
+            <hr />
+            <a href="#top" className='text-center' style={{color:"black", fontSize:"60px"}}> Return to Top <ArrowUpwardIcon sx={{fontSize:"60px"}}/></a>
+            <hr />
         </div>
     )}
 
