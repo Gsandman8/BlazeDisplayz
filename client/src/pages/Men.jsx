db41d08aa1403def34b39ef28b2b4805c24059b2
Gsandman8 <gage.e.sandman@gmail.com>
2024-01-27 21:10:39 -0800

    Added more styling to men's navbar

--- client/src/pages/Men.jsx
+++ client/src/pages/Men.jsx
@@ -3,32 +3,36 @@ import ProductList from '../components/ProductList';
 import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_CURRENT_TAG } from '../utils/actions';
 import { Link } from 'react-router-dom';
+import  ManIcon  from '@mui/icons-material/Man';
 
 
 const Men = () => {
-
     const [state, dispatch] = useStoreContext();
     const tag = state.currentTag || 'Clothing';
 
     
     return (
         <div className="container row">
-            <h1 className="text-center">Men's {tag}</h1>
-            <hr />
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg justify-content-between">
+            <h1 className="text-center">Men's {tag} <ManIcon/></h1>
+                <Link onClick={()=>dispatch({
+                    type: UPDATE_CURRENT_TAG,
+                    currentTag: 'Clothing'
+                })} style={{color:"black"}}>All Clothing</Link>
                 <Link onClick={()=>dispatch({
                     type: UPDATE_CURRENT_TAG,
                     currentTag: 'Hoodies'
-                })}>Hoodies</Link>
+                })} style={{color:"black"}}>Hoodies</Link>
                 <Link onClick={()=>dispatch({
                     type: UPDATE_CURRENT_TAG,
                     currentTag: 'Pants'
-                })}>Pants</Link>
+                })} style={{color:"black"}}>Pants</Link>
                 <Link onClick={()=>dispatch({
                     type: UPDATE_CURRENT_TAG,
                     currentTag: 'Shoes'
-                })}>Shoes</Link>
+                })} style={{color:"black"}}>Shoes</Link>
             </nav>
+            <hr />
             <ProductList />
         </div>
     )}
