97791a073a4ee8a11aa12b6e0fa91122b21fba81
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2024-01-28 23:07:51 -0800

    improved drawer behavior and removed submenu

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -21,6 +21,15 @@ export default function Nav() {
     setDrawerOpen(!drawerOpen);
   };
 
+  const handleDrawerLinkClick = (category) => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: category,
+    });
+  
+    setDrawerOpen(false);
+  };
+
   const handleMouseEnter = (category) => {
     if (!drawerOpen) {
       if (category === 'mens') {
@@ -77,11 +86,11 @@ export default function Nav() {
       })}>
             MENS
           </Link>
-          <ul id='menuList' className={`${isMensHovering ? '' : 'hidden'}`}>
+          {/* <ul id='menuList' className={`${isMensHovering ? '' : 'hidden'}`}>
             <li><Link> HOODIES </Link></li>
             <li><Link> PANTS </Link></li>
             <li><Link> SHOES </Link></li>
-          </ul>
+          </ul> */}
         </li>
         <li className="nav-item" 
         onMouseEnter={() => handleMouseEnter('womens')} 
@@ -92,11 +101,11 @@ export default function Nav() {
       })}>
             WOMENS
           </Link>
-          <ul id='menuList' className={`${isWomensHovering ? '' : 'hidden'}`}>
+          {/* <ul id='menuList' className={`${isWomensHovering ? '' : 'hidden'}`}>
             <li><Link> HOODIES </Link></li>
             <li><Link> PANTS </Link></li>
             <li><Link> SHOES </Link></li>
-          </ul>
+          </ul> */}
         </li>
         <li className="nav-item" 
         onMouseEnter={() => handleMouseEnter('kids')} 
@@ -107,11 +116,11 @@ export default function Nav() {
       })}>
             KIDS
           </Link>
-          <ul id='menuList' className={`${isKidsHovering ? '' : 'hidden'}`}>
+          {/* <ul id='menuList' className={`${isKidsHovering ? '' : 'hidden'}`}>
             <li><Link> HOODIES </Link></li>
             <li><Link> PANTS </Link></li>
             <li><Link> SHOES </Link></li>
-          </ul>
+          </ul> */}
         </li>
       </ul>
 
@@ -121,52 +130,65 @@ export default function Nav() {
         onClose={handleDrawerToggle}
         className="responsive-drawer"
       >
-      <IconButton
-        className="close-button"
-        onClick={handleDrawerToggle}
-        aria-label="close"
-      >
-        <CloseIcon fontSize="large"/>
-      </IconButton>
+        <IconButton
+          className="close-button"
+          onClick={handleDrawerToggle}
+          aria-label="close"
+        >
+          <CloseIcon fontSize="large"/>
+        </IconButton>
 
-      <section className='sideBarList'>
-        <ul>
-          <li><Link to='/newarrivals'>NEW ARRIVAL</Link></li>
-          <li>
-            <Link to='/men' onClick={()=>dispatch({
-            type: UPDATE_CURRENT_CATEGORY,
-            currentCategory: "Men"
-      })}> MENS </Link>
-            <ul className='sideBarMenu'>
-            <li><Link> HOODIES </Link></li>
-              <li><Link> PANTS </Link></li>
-              <li><Link> SHOES </Link></li>
-              </ul>
+        <section className='sideBarList'>
+          <ul>
+            <li>
+              <Link 
+              to='/newarrivals' 
+              onClick={() => handleDrawerLinkClick('New Arrivals')}
+              >
+                NEW ARRIVAL
+              </Link>
             </li>
-          <li>
-            <Link to='/women' onClick={()=>dispatch({
-            type: UPDATE_CURRENT_CATEGORY,
-            currentCategory: "Women"
-      })}> WOMENS </Link>
-            <ul className='sideBarMenu'>
-              <li><Link> HOODIES </Link></li>
-              <li><Link> PANTS </Link></li>
-              <li><Link> SHOES </Link></li>
-            </ul>
-          </li>
-          <li>
-            <Link to='/kids' onClick={()=>dispatch({
-            type: UPDATE_CURRENT_CATEGORY,
-            currentCategory: "Children"
-      })}> KIDS </Link>
-            <ul className='sideBarMenu'>
-              <li><Link> HOODIES </Link></li>
-              <li><Link> PANTS </Link></li>
-              <li><Link> SHOES </Link></li>
-            </ul>  
-          </li>
-        </ul>
-      </section>
+            <li>
+              <Link 
+              to='/men' 
+              onClick={() => handleDrawerLinkClick('Men')}
+              >
+                MENS
+              </Link>
+              {/* <ul className='sideBarMenu'>
+                <li><Link> HOODIES </Link></li>
+                <li><Link> PANTS </Link></li>
+                <li><Link> SHOES </Link></li>
+              </ul> */}
+            </li>
+            <li>
+              <Link 
+              to='/women' 
+              onClick={() => handleDrawerLinkClick('Women')}
+              >
+                WOMENS
+              </Link>
+              {/* <ul className='sideBarMenu'>
+                <li><Link> HOODIES </Link></li>
+                <li><Link> PANTS </Link></li>
+                <li><Link> SHOES </Link></li>
+              </ul> */}
+            </li>
+            <li>
+              <Link 
+              to='/kids' 
+              onClick={() => handleDrawerLinkClick('Children')}
+              >
+                KIDS
+              </Link>
+              {/* <ul className='sideBarMenu'>
+                <li><Link> HOODIES </Link></li>
+                <li><Link> PANTS </Link></li>
+                <li><Link> SHOES </Link></li>
+              </ul>   */}
+            </li>
+          </ul>
+        </section>
       </Drawer>
     </nav>
   );
