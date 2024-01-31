952bb766441f5eff4e9bfd713a0fe4d9312c4547
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2024-01-30 20:04:35 -0800

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Set the timeout value in milliseconds (e.g., 30 seconds)
+}
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/BlazeDisplayz');
 
 module.exports = mongoose.connection;
